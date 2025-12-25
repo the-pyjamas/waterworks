4bda7b522672c0dbddc061ada9e8ced7340b730a
richie <richiepagard@gmail.com>
2025-12-26 02:57:01 +0330

    feat; Implement the basic model for for user-profile update.

---
+++ yazd_linux/accounts/forms/user_profile_form.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+from accounts.models import UserProfile
+
+
+User = get_user_model()
+
+
+class UserProfileUpdateForm(forms.ModelForm):
+    """
+    Represent updating user profile.
+    Just profile info.
+    Validate the incoming data from the client and cleaned them.
+    """
+    class Meta:
+        model = UserProfile
+        fields = ("first_name", "last_name", "address")
+
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": "form-control"
+            })
