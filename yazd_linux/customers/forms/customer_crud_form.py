287254965f08d6e0a0429e6cecaad28cff438d5c
richie <richiepagard@gmail.com>
2026-01-02 01:32:09 +0330

    feat; Implement basic form for Customers CRUD.
    - Implement the basic form for customers crud.
    
    - Updates the sidebar of the `base` template to showing Customers section.

---
+++ yazd_linux/customers/forms/customer_crud_form.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from customers.models import Customer
+
+
+class CreateCustomerForm(forms.ModelForm):
+    """
+    Creation form for creating a new customer object.
+    Only the admin-user and technician-user be able to
+    create a new customer.
+    """
+    class Meta:
+        model = Customer
+        fields = (
+            "user", "vendor", "technician","description",
+            "installation_date", "next_inspection_date", "replacement_date"
+        )
+        widgets = {
+            "installation_date": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+            "next_inspection_date": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+            "replacement_date": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialized the form class.
+
+        Add some same widgets such as 'CSS class' to the all fields,
+        or any other same properties.
+        """
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": "form-control"
+            })
