af734d1dd0430ed04792844a4f09e773b153373f
richie <richiepagard@gmail.com>
2026-01-04 14:12:52 +0330

    refactor; Add extra fields to the list-display of Technician admin panel.

--- yazd_linux/technicians/admin.py
+++ yazd_linux/technicians/admin.py
@@ -8,14 +8,17 @@ from .models import Technician
 class TechnicianAdmin(admin.ModelAdmin):
     list_display = (
         "user__phone_number",
-        "user__userprofile__address",
+        "national_code",
+        "installer_code",
         "is_active"
     )
-    list_filter = ("is_active", "created_at", "updated_at")
+    list_filter = ("is_active", "created_at", "updated_at", "skill_level")
     search_fields = (
         "user__phone_number",
         "user__username",
         "user__userprofile__address",
+        "national_code",
+        "installer_code"
     )
     readonly_fields = ("created_at", "updated_at")
     ordering = ("user",)
