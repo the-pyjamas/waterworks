a2aa7ba310575ea5469a63dcf505cfe67bb7f705
richie <richiepagard@gmail.com>
2026-02-12 18:48:03 +0330

    feat; Configure `search-filter` on the Customers List view and the customers-list template.

--- yazd_linux/customers/views/customer_list.py
+++ yazd_linux/customers/views/customer_list.py
@@ -2,18 +2,36 @@ from django.shortcuts import render
 from django.views import View
 
 from customers.models import Customer
+from customers.filters import CustomerFilter
 
 
 class CustomerListView(View):
     """
-    List all active customers.
+    List all customers.
+
+    Methods
+        get(GET HTTP).
     """
     template_name = "customers/customers_list.html"
 
     def get(self, request):
+        """
+        Lists all the customers,
+        filters them base on the client request, the search filter.
+        """
         customers = Customer.objects.filter(is_active=True)
+        # Search filter according to the 'CustomerFilter'
+        filters = CustomerFilter(
+            request.GET,
+            queryset=Customer.objects.all()
+        )
+        # Remake the quesryset to the requests of the search filters
+        customers = filters.qs
 
-        context = {"customers": customers}
+        context = {
+            "customers": customers,
+            "filters": filters
+        }
         return render(
             request=request,
             template_name=self.template_name,
