fc5a773d85763ae1144630e44b3cd7a5a9d99891
richie <richiepagard@gmail.com>
2026-02-12 18:46:12 +0330

    feat; Add `form-control` class to the Customer Filter.

--- yazd_linux/customers/filters.py
+++ yazd_linux/customers/filters.py
@@ -1,11 +1,30 @@
+from django.forms import Select
+
 import django_filters
 
+from devices.models import Device
+from technicians.models import Technician
+from vendors.models import Vendor
 from customers.models import Customer
+
 
 class CustomerFilter(django_filters.FilterSet):
     """
     Search filter on customers.
     """
+    device = django_filters.ModelChoiceFilter(
+        queryset=Device.objects.filter(is_active=True),
+        widget=Select(attrs={"class": "form-control"})
+    )
+    technician = django_filters.ModelChoiceFilter(
+        queryset=Technician.objects.filter(is_active=True),
+        widget=Select(attrs={"class": "form-control"})
+    )
+    vendor = django_filters.ModelChoiceFilter(
+        queryset=Vendor.objects.filter(is_active=True),
+        widget=Select(attrs={"class": "form-control"})
+    )
+
     class Meta:
         model = Customer
         fields = ("device", "technician", "vendor")
