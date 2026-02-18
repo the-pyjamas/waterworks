ce09f0fe658bfc3980f98b4efc3cbaa08eab5db4
richie <richiepagard@gmail.com>
2026-02-18 22:14:34 +0330

    feat; Implement the custom search filter for Vendors base on the shop-name and contact-phone.

---
+++ yazd_linux/vendors/filters.py
@@ -0,0 +1,21 @@
+from django.forms import TextInput
+
+import django_filters
+
+from vendors.models import Vendor
+
+
+class VendorFilter(django_filters.FilterSet):
+    """
+    Search filter on vendors.
+    """
+    shop_name = django_filters.CharFilter(
+        widget=TextInput(attrs={"class": "form-control"})
+    )
+    contact_phone = django_filters.CharFilter(
+        widget=TextInput(attrs={"class": "form-control"})
+    )
+
+    class Meta:
+        model = Vendor
+        fields = ("shop_name", "contact_phone")
