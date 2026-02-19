268cde1846e79128efde9179b35b63e3b5fc3151
richie <richiepagard@gmail.com>
2026-02-19 18:58:17 +0330

    feat; List last five customers in the buttom section of the Home Page/dashboard.

--- yazd_linux/dashboard/views.py
+++ yazd_linux/dashboard/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 
+from customers.models import Customer
+
 
 class DashboardView(View):
     template_name = "dashboard/dashboard.html"
     
     def get(self, request):
-        context = {"name": "Richie"}
+        last_five_customers = Customer.last_objects(5)
 
+        context = {
+            "last_five_customers": last_five_customers
+        }
         return render(
             template_name=self.template_name,
             request=request,
