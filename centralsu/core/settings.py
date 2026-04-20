0154b9ed4f1d9eb34b5dd4c99fc7b54f7da0f6a7
richie <richiepagard@gmail.com>
2026-04-20 23:47:42 +0330

    feat; Add server IP to the ALLOWED_HOSTS.

--- centralsu/core/settings.py
+++ centralsu/core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-7nf&f19b3#)8yv1a1vx8=xy5t1*aqz#s$_=nwycs@)kwm()(oo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['193.242.208.24']
 
 
 # Application definition
