971f01c0db75f7aed096cdb100b6dc8e4bba4416
richie <richiepagard@gmail.com>
2026-04-22 12:44:24 +0330

    feat; Add list and create device views to URL patterns.

--- centralsu/devices/urls.py
+++ centralsu/devices/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import DeviceCreateView
+from .views import DeviceCreateView, DevicesListView
 
 
 app_name = "devices"
 
 CRUD_URLS = [
 	path("create/", DeviceCreateView.as_view(), name="device-create"),
+	path("list/", DevicesListView.as_view(), name="devices-list"),
 ]
 urlpatterns = CRUD_URLS
