fd2c47c024fe23fb26d92d42294b75c3b50c4920
richie <richiepagard@gmail.com>
2026-04-22 18:06:57 +0330

    fix; Fix the customers lists for technicians.
    - Fix the customers list page to just show customers to a technician who is the installer of their device.

--- centralsu/customers/views/customer_list.py
+++ centralsu/customers/views/customer_list.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from customers.models import Customer
@@ -14,16 +14,41 @@ class CustomerListView(View):
     """
     template_name = "customers/customers_list.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        """
+		Ensures that the user who creating a new user
+		has an appropriate role.
+		"""
+        user = request.user
+        is_allowed = (
+            user.is_superuser
+            or user.role == 'Admin'
+            or user.role == 'Technician'
+        )
+
+        # Redirects user only if user is neither
+        # a superuser nor has an authorized role
+        if not is_allowed:
+            return redirect('accounts:user-dashboard')
+        
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         """
         Lists all the customers,
         filters them base on the client request, the search filter.
         """
-        customers = Customer.objects.filter(is_active=True)
+        user = request.user
+
+        if user.role == 'Technician':
+            customers = Customer.objects.filter(technician__user_id=user.id)
+        else:
+            customers = Customer.objects.filter(is_active=True)
+
         # Search filter according to the 'CustomerFilter'
         filters = CustomerFilter(
             request.GET,
-            queryset=Customer.objects.all()
+            queryset=customers
         )
         # Remake the quesryset to the requests of the search filters
         customers = filters.qs
