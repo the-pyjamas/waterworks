d754dcb442fe108ec9af20ee4b29631de2b687da
richie <richiepagard@gmail.com>
2026-06-21 18:23:13 -0400

    feat; Implement technician update endpoint.
    - Implement a new view for technicians to update a technician instance which inherits from BaseRoleProfileUpdateView.
    
    - Add technician update view to the technicians endpoint list.

--- centralsu/apps/technicians/views/__init__.py
+++ centralsu/apps/technicians/views/__init__.py
@@ -5,3 +5,4 @@ from .technician_create_view import (
 )
 from .technician_list_view import TechnicianListView
 from .technician_retrieve_view import TechnicianRetrieveView
+from .technician_update_view import TechnicianUpdateView
